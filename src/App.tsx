ae6ee7627df0490d7d467dec12d468566eb0e148
Esteban Robert <esteban.robert23@gmail.com>
2024-09-10 11:16:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'flowbite-react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Juzgado query</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
