692316112eba76bba9b1844930e4a0a95fc0ed92
Nahuewe <nahuelsoriap@gmail.com>
2025-07-16 03:41:59 -0300

    Corregido bug inesperado a las 3 de la mañana con el paginado

--- src/shared/helpers/setUrlParams.ts
+++ src/shared/helpers/setUrlParams.ts
@@ -1,11 +1,13 @@
 export const setUrlParams = (filters: object) => {
   const params = new URLSearchParams()
-  
+
   Object.entries(filters).forEach(([key, value]) => {
-    if (value) {
+    if (key === 'paginate' && value === 'false') return
+
+    if (value !== undefined && value !== null && value !== '' && value !== false) {
       params.append(key, value.toString())
     }
   })
-  
+
   return params
 }
